d1348f040bf429646220a561119216df10e55c12
jinyung2 <jinyoungchoi118@gmail.com>
2021-03-30 12:23:25 -0700

    added some more styling

--- src/components/Table.js
+++ src/components/Table.js
@@ -7,7 +7,7 @@ function TableHeader() {
             <tr>
                 <th>Name</th>
                 <th>Job</th>
-                <th>Remove</th>
+                <th></th>
             </tr>
         </thead>
     );
